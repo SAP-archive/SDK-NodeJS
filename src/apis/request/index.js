a83253b2e8250cde9da1c655c16a6e223346144a
Marian ANDRE <mandre@student.42.fr>
2017-03-08 16:35:41 +0100

    [UPD] refactor

--- src/apis/request/index.js
+++ src/apis/request/index.js
@@ -1,11 +1,84 @@
-import Request from './request'
-import Converse from './converse'
+import superagent from 'superagent'
+import superagentProxy from 'superagent-proxy'
+import superagentPromise from 'superagent-promise'
 
-export default class RequestAPI {
+import constants from '../constants'
+import { Conversation, Response, RecastError } from '../resources'
 
-  constructor (token) {
-    this.request = new Request(token)
-    this.converse = new Converse(token)
+const agent = superagentPromise(superagentProxy(superagent), Promise)
+
+export default class Request {
+
+  constructor (token, language) {
+    this.token = token
+    this.language = language
+  }
+
+  /*
+   * /request (with text string)
+   */
+  analyseText = async (text, options = {}) => {
+    const token = options.token || this.token
+    const proxy = options.proxy
+    const data = { text, language: options.language || this.language }
+    if (!token) { throw new RecastError('Parameter token is missing') }
+
+    try {
+      const request = agent('POST', constants.REQUEST_ENDPOINT)
+        .set('Authorization', `Token ${this.token}`)
+      if (proxy) { request.proxy(proxy) }
+
+      const res = await request.send(data)
+      return new Response(res.body.results)
+    } catch (err) {
+      throw new RecastError(err)
+    }
+  }
+
+  /*
+   * /request (with audio file)
+   */
+  analyseFile = async (file, options = {}) => {
+    const token = options.token || this.token
+    const proxy = options.token
+    if (!token) { throw new RecastError('Parameter token is missing') }
+
+    try {
+      const request = agent('POST', constants.REQUEST_ENDPOINT)
+        .set('Authorization', `Token ${this.token}`)
+      if (proxy) { request.proxy(proxy) }
+
+      const res = await request.attach('voice', file).send()
+      return new Response(res.body.results)
+    } catch (err) {
+      throw new RecastError(err)
+    }
+  }
+
+  /*
+   * /converse
+   */
+  converseText = async (text, options = {}) => {
+    const token = options.token || this.token
+    const proxy = options.proxy
+    const data = {
+      text,
+      language: options.language || this.language,
+      conversation_token: options.conversationToken,
+      memory: options.memory,
+    }
+    if (!token) { throw new RecastError('Parameter token is missing') }
+
+    try {
+      const request = agent('POST', constants.CONVERSE_ENDPOINT)
+        .set('Authorization', `Token ${this.token}`)
+      if (proxy) { request.proxy(proxy) }
+
+      const res = await request.send(data)
+      return new Conversation({ ...res.body.results, recastToken: this.token })
+    } catch (err) {
+      throw new RecastError(err.message)
+    }
   }
 
 }
