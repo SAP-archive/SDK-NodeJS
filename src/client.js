2afc9ff2cb8e12b8b11b9ed5113806234241ba92
Jasmine Anteunis <jasmine.anteunis@gmail.com>
2017-11-23 23:31:05 +0100

    fix(build) import in client.js

--- src/client.js
+++ src/client.js
@@ -5,6 +5,7 @@ export default class Client {
   constructor (token, language, userSlug, botSlug) {
     this.connect = new apis.connect(token, language)
     this.request = new apis.request(token, language)
+    this.build = new apis.build(token, language)
 
     if (userSlug && botSlug) {
       this.train = new apis.train(token, language, userSlug, botSlug)
@@ -16,5 +17,7 @@ export default class Client {
   static request = apis.request
 
   static train = apis.train
+
+  static build = apis.build
 
 }
